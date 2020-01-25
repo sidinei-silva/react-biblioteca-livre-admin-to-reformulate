6500f7a8f967391f8af4f2322c5caf0125a5ea30
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-25 14:32:51 -0300

    Config structure

---
+++ src/pages/main/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class Main extends Component {
+
+  render(){
+    return(
+      <div className='app'>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
