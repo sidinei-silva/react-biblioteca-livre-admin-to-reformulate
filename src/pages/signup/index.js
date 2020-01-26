7bd5e62a0cdd069d8d0198be3bc8857ba2610838
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-26 09:57:09 -0300

    Include function to register admin

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 import Logo from "../../assets/logo.svg";
 
-import { Form, Container, Body } from "./styles";
+import { Form, Container } from "./styles";
+
+import api from "../../services/api";
 
 class SignUp extends Component {
   state = {
@@ -13,41 +15,51 @@ class SignUp extends Component {
     error: ""
   };
 
-  handleSignUp = e => {
+  // ...
+  handleSignUp = async e => {
     e.preventDefault();
-    alert("Eu vou te registrar");
+    const { username, email, password } = this.state;
+    if (!username || !email || !password) {
+      this.setState({ error: "Preencha todos os dados para se cadastrar" });
+    } else {
+      try {
+        await api.post("/admins/", { username, email, password });
+        this.props.history.push("/login");
+      } catch (err) {
+        console.log(err);
+        this.setState({ error: "Ocorreu um erro ao registrar sua conta." });
+      }
+    }
   };
 
   render() {
     return (
-      <Body>
-          <Container>
-            <Form onSubmit={this.handleSignUp}>
-              <img src={Logo} alt="Airbnb logo" />
-              {this.state.error && <p>{this.state.error}</p>}
-              <input
-                type="text"
-                placeholder="Nome de usuário"
-                onChange={e => this.setState({ username: e.target.value })}
-              />
-              <input
-                type="email"
-                placeholder="Endereço de e-mail"
-                onChange={e => this.setState({ email: e.target.value })}
-              />
-              <input
-                type="password"
-                placeholder="Senha"
-                onChange={e => this.setState({ password: e.target.value })}
-              />
-              <button type="submit">Cadastrar grátis</button>
-              <hr />
-              <Link to="/">Fazer login</Link>
-            </Form>
-        </Container>
-      </Body>
+      <Container>
+        <Form onSubmit={this.handleSignUp}>
+          <img src={Logo} alt="Airbnb logo" />
+          {this.state.error && <p>{this.state.error}</p>}
+          <input
+            type="text"
+            placeholder="Nome de usuário"
+            onChange={e => this.setState({ username: e.target.value })}
+          />
+          <input
+            type="email"
+            placeholder="Endereço de e-mail"
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={e => this.setState({ password: e.target.value })}
+          />
+          <button type="submit">Cadastrar grátis</button>
+          <hr />
+          <Link to="/">Fazer login</Link>
+        </Form>
+      </Container>
     );
   }
 }
 
-export default SignUp;
+export default withRouter(SignUp);
