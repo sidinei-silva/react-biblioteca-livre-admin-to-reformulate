ab0348255a784c7223cae680c9c4381c09f31692
Sidinei Silva <sidinei.dev@icloud.com>
2020-01-26 09:55:52 -0300

    Insert /api in endpoint api service

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:3333"
+  baseURL: "http://127.0.0.1:3333/api"
 });
 
 api.interceptors.request.use(async config => {
